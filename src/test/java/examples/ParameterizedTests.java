09b42fde1745cc5eb13fd3e6d05953865500f3de
stannyweb <stannyweb@gmail.com>
2025-03-07 17:09:31 +0200

    feat: Add parameterized test for zip code API
    This commit adds a parameterized test to verify the place name for different zip codes using JUnit 5's @MethodSource.
    
    Changes:
    - Added ParameterizedTests class with a parameterized test method.
    - Implemented zipCodeSource() method to provide test data using Stream<Arguments>.
    - Corrected JSONPath in the test to select the place name string correctly.
    
    This commit improves the test coverage and demonstrates how to use parameterized tests with @MethodSource.

---
+++ src/test/java/examples/ParameterizedTests.java
@@ -0,0 +1,31 @@
+package examples;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ParameterizedTests {
+
+    @ParameterizedTest
+    @MethodSource(value = "zipCodeSource")
+    void requestZipCodeFromCollection_ExpectSpecifiedPlaceName(String countryCode, String zipCode,
+                                                               String expectedPlaceName) {
+
+        given().pathParams("countryCode", countryCode, "zipCode", zipCode).when().get("http://zippopotam" + ".us" +
+                "/{countryCode}/{zipCode}").then().assertThat().body("places[0].'place name'",
+                equalTo(expectedPlaceName));
+    }
+
+    static Stream<Arguments> zipCodeSource() {
+        return Stream.of(
+                Arguments.of("us", "90210", "Beverly Hills"),
+                Arguments.of("us", "12345", "Schenectady"),
+                Arguments.of("ca", "B2R", "Waverley")
+        );
+    }
+}

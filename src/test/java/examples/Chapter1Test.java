44502222e145fc5cc6958db7f59c8fdcd8299654
stannyweb <stannyweb@gmail.com>
2025-03-07 16:10:02 +0200

    REST Assured test
     - test to verify the place name for a given zip code.

--- src/test/java/examples/Chapter1Test.java
+++ src/test/java/examples/Chapter1Test.java
@@ -2,7 +2,7 @@ package examples;
 
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class Chapter1Test {
@@ -10,8 +10,19 @@ public class Chapter1Test {
     @Test
     void requestZipCode6000_checkPlaceNameInResponseBody_expectStaraZagora() {
 
-        given().when().get("http://zippopotam.us/BG/6000").then().assertThat().body("places[0].'place name'", equalTo("Стара Загора / Stara Zagora"));
+        given().when().get("http://zippopotam.us/BG/6000").then().assertThat().log().body(
+        ).body("places.'longitude'", equalTo("25.6419"));
 
+    }
 
+    @Test
+    void requestZipCode6000_checkListOfPlacesInResponseBody() {
+        given().when().get("http://zippopotam.us/BG/6000").then().assertThat().body("places.'place name'", hasItem(
+                "Стара Загора / Stara Zagora"));
+    }
+
+    @Test
+    void requestZipCode6000_checkListOfPlacesInResponseBody_expectOne() {
+        given().when().get("http://zippopotam.us/BG/6000").then().assertThat().body("places.'place name'", hasSize(1));
     }
 }
